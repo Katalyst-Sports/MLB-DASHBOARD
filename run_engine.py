29a5803896e78696f52188acaa4ec1911dbb54ed
Katalyst-Sports <k8piler@gmail.com>
2026-04-11 08:57:38 -0500

    Update run_engine.py

--- run_engine.py
+++ run_engine.py
@@ -263,8 +263,8 @@ Games:
 {games_text}
 """
 
-    response = client.chat.completions.create(
-    model="llama3-8b-8192",
+   response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
     messages=[{"role": "user", "content": prompt}],
     temperature=0.6
 )
